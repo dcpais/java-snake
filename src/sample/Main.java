74faa4656c80521eee23c7228b6cceb6ded06363
dcpais <dpaismail@gmail.com>
2021-07-05 16:55:40 +1000

    Gui class setup

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,17 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.userinterface.Gui;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        Gui userInterface = new Gui(primaryStage);
     }
-
 
     public static void main(String[] args) {
         launch(args);
